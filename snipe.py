7b6c705d6b1bf47b5dd523b7d151f71111af63fd
Ian Perry <ian.perry@avionics411.com>
2025-08-22 10:32:30 -0500

    Modified

--- snipe.py
+++ snipe.py
@@ -1,57 +1,313 @@
+from __future__ import annotations
 import requests
 import time
 import base64
 import pprint
 import re
 import logging
+import json
 from colorama import Fore
 from colorama import Style
+from pydantic import BaseModel
+from typing import Optional, Union, List, Dict
+
 
 logger = logging.getLogger("MosyleSnipeSync")
 
-class User:
+class RowResult[T]:
+    def __init__(self, total: int = 0, rows: list[T] = []):
+        self.total = total
+        self.rows = rows
+
+class Timestamp(BaseModel):
+    date: str
+    formatted: str
+
+class Maintenances:
+    pass # TBD
+
+class AvailableActions(BaseModel):
+    update: Optional[bool]
+    delete: Optional[bool]
+    clone: Optional[bool]
+    restore: Optional[bool]
+    checkout: Optional[bool]
+    checkin: Optional[bool]
+    audit: Optional[bool]
+    bulk_selectable: Optional[AvailableActions] = None
+
+class Department(BaseModel):
+    id: int
+    name: str
+    phone: Optional[str]
+    fax: Optional[str]
+    image: Optional[str]
+    email: Optional[str]
+    company: Optional[Company]
+    manager: Optional[User]
+    location: Optional[Location]
+    users_count: str = "0"
+    notes: str = ""
+    created_at: Optional[Timestamp]
+    created_by: Optional[User]
+    updated_at: Optional[Timestamp]
+    available_actions: AvailableActions
+
+class License(BaseModel):
+            id: int
+            name: str
+            company: Optional[Company]
+            manufacturer: Optional[Manufacturer]
+            product_key: Optional[str]
+            order_number: Optional[str]
+            purchase_order: Optional[str]
+            purchase_date: Optional[Timestamp]
+            termination_date: Optional[Timestamp]
+            depreciation: Optional[Depreciation]
+            purchase_cost: Optional[float]
+            purchase_cost_numeric: Optional[str]
+            notes: str = ""
+            expiration_date: Optional[Timestamp]
+            seats: int = 0
+            free_seats_count: int = 0
+            remaining: int = 0
+            min_amt: int | None = 0
+            license_name: Optional[str]
+            license_email: Optional[str]
+            reassignable: bool = False
+            maintained: bool = False
+            supplier: Optional[Supplier]
+            category: Optional[Category]
+            created_by: Optional[User]
+            created_at: Optional[Timestamp]
+            updated_at: Optional[Timestamp]
+            deleted_at: Optional[Timestamp]
+            user_can_checkout: bool = False
+            available_actions: AvailableActions
+
+class User(BaseModel):
     pass
 
-class Manufacturer:
-    def __init__(self, id: int, name: str):
-        self.id = id
-        self.name = name
+class Manufacturer(BaseModel):
+    id: int
+    name: str
 
-class Model:
-    def __init__(
-            self, 
-            id: int, 
-            name: str, 
-            manufacturer: Manufacturer | None = None,
-            image: str | None = None,
-        ):
-        self.id = id
-        self.name = name
-        self.model_number = model_number
-        self.category_id = category
-    )
+class Field(BaseModel):
+    id: int
+    name: str
+    db_column_name: Optional[str]
+    format: Optional[str]
+    field_values: Optional[str]
+    field_values_array: Optional[list[str]]
+    type: Optional[str]
+    required: bool = False
+    display_in_user_view: bool = False
+    auto_add_to_fieldsets: bool = False
+    show_in_listview: bool = False
+    display_checkin: bool = False
+    display_checkout: bool = False
+    display_audit: bool = False
+    created_at: Optional[Timestamp]
+    updated_at: Optional[Timestamp]
 
-class Asset:
-    def __init__(
-            self,
-            id: int,
-            serial: str,
-            model: str,
-            asset_tag: str,
-            payload: dict[str,str]
-        ):
-        self.serial = serial
-        self.model = model
-        self.asset_tag = asset_tag
-        self.payload = payload
+class Fieldset(BaseModel):
+    id: int
+    name: str
+    description: Optional[str]
+    fields: RowResult[Field]
+    created_by: Optional[User]
+    created_at: Optional[Timestamp]
+    updated_at: Optional[Timestamp]
 
-    def __repr__(self):
-        return f"Asset(serial={self.serial}, model={self.model}, asset_tag={self.asset_tag}, payload={self.payload})"
+class Depreciation:
+    pass
+
+class Category(BaseModel):
+            id: int
+            name: str
+            image: Optional[str]
+            category_type: Optional[str]
+            has_eula: bool = False
+            use_default_eula: bool = False
+            eula: Optional[str]
+            checkin_email: bool = False
+            require_acceptance: bool = False
+            item_count: int = 0
+            assets_count: int = 0
+            accessories_count: int = 0
+            consumables_count: int = 0
+            components_count: int = 0
+            licenses_count: int = 0
+            notes: str = ""
+            created_by: Optional[User]
+            created_at: Optional[Timestamp]
+            updated_at: Optional[Timestamp]
+            available_actions: AvailableActions
+
+class FieldsetValue(BaseModel):
+    field: Field
+    default: Optional[str]
+
+class Model(BaseModel):
+            id: int 
+            name: str 
+            manufacturer: Optional[Manufacturer]
+            image: Optional[str]
+            model_number: Optional[str]
+            min_amt: Optional[int]
+            remaining: Optional[int]
+            depreciation: Optional[Depreciation]
+            assets_count: int = 0
+            category: Optional[Category]
+            fieldset: Optional[Fieldset]
+            default_fieldset_values: list[FieldsetValue] = []
+            eol: Optional[str]
+            requestable: bool = False
+            notes: str = ""
+            created_by: Optional[User]
+            created_at: Optional[Timestamp]
+            updated_at: Optional[Timestamp]
+            deleted_at: Optional[Timestamp]
+            available_actions: AvailableActions
+
+class Status(BaseModel):
+            id: int
+            name: str
+            type: str
+            color: Optional[str]
+            show_in_nav: bool = False
+            default_label: bool = False
+            assets_count: int = 0
+            notes: str = ""
+            created_by: Optional[User]
+            created_at: Optional[Timestamp]
+            updated_at: Optional[Timestamp]
+            available_actions: AvailableActions
+
+class Supplier(BaseModel):
+    id: int
+    name: str
+    image: Optional[str]
+    url: Optional[str]
+    address: Optional[str]
+    address2: Optional[str]
+    city: Optional[str]
+    state: Optional[str]
+    country: Optional[str]
+    zip: Optional[str]
+    fax: Optional[str]
+    phone: Optional[str]
+    email: Optional[str]
+    contact: Optional[str]
+    assets_count: int = 0
+    accessories_count: int = 0
+    licenses_count: int = 0
+    consumables_count: int = 0
+    components_count: int = 0
+    notes: str = ""
+    created_at: Optional[Timestamp]
+    created_by: Optional[User]
+    updated_at: Optional[Timestamp]
+    available_actions: AvailableActions
+
+class Company(BaseModel):
+    id: int
+    name: str
+    phone: Optional[str]
+    fax: Optional[str]
+    email: Optional[str]
+    image: Optional[str]
+    assets_count: int = 0
+    licenses_count: int = 0
+    accessories_count: int = 0
+    consumables_count: int = 0
+    components_count: int = 0
+    users_count: int = 0
+    created_by: Optional[User]
+    created_at: Optional[Timestamp]
+    updated_at: Optional[Timestamp]
+    notes: str = ""
+    available_actions: AvailableActions
+
+class Location(BaseModel):
+    id: int
+    name: str
+    image: Optional[str]
+    address: Optional[str]
+    address2: Optional[str]
+    city: Optional[str]
+    state: Optional[str]
+    country: Optional[str]
+    zip: Optional[str]
+    phone: Optional[str]
+    fax: Optional[str]
+    accessories_count: int = 0
+    assigned_accessories_count: int = 0
+    assets_count: int = 0
+    assigned_assets_count: int = 0
+    rtd_assets_count: int = 0
+    users_count: int = 0
+    currency: Optional[str]
+    ldap_ou: Optional[str]
+    notes: str = ""
+    created_at: Optional[Timestamp]
+    created_by: Optional[User]
+    updated_at: Optional[Timestamp]
+    parent: Optional[Location]
+    manager: Optional[User]
+    company: Optional[Company]
+    children: list[Location] = []
+    available_actions: AvailableActions
+
+class Asset(BaseModel):
+    id: int
+    name: Optional[str]
+    asset_tag: Optional[str]
+    serial: Optional[str]
+    model: Optional[Model]
+    model_number: Optional[str]
+    status_label: Optional[Status]
+    category: Optional[Category]
+    manufacturer: Optional[Manufacturer]
+    supplier: Optional[Supplier]
+    notes: str = ""
+    order_number: Optional[str]
+    byod: bool = False
+    eol: Optional[str]
+    asset_eol_date: Optional[Timestamp]
+    company: Optional[str]
+    location: Optional[Location]
+    rtd_location: Optional[Location]
+    image: Optional[str]
+    qr: Optional[str]
+    alt_barcode: Optional[str]
+    assigned_to: Optional[User]
+    warranty_months: Optional[int]
+    warranty_expires: Optional[Timestamp]
+    created_by: Optional[User]
+    created_at: Optional[Timestamp]
+    updated_at: Optional[Timestamp]
+    last_audit_date: Optional[Timestamp]
+    next_audit_date: Optional[Timestamp]
+    deleted_at: Optional[Timestamp]
+    purchase_date: Optional[Timestamp]
+    agent: Optional[str]
+    last_checkout: Optional[Timestamp]
+    last_checkin: Optional[Timestamp]
+    expected_checkin: Optional[Timestamp]
+    purchase_cost: Optional[int]
+    checkin_counter: Optional[int]
+    checkout_counter: Optional[int]
+    requests_counter: Optional[int]
+    user_can_checkout: bool = True
+    book_value: Optional[int]
+    custom_fields: Optional[dict[str, Field]]
+    available_actions: AvailableActions
 
 class Snipe:
     def __init__(
             self, 
-            snipe_token:         str,
+            snipe_token:        str,
             url:                str,
             manufacturer_id:    int,
             macos_category_id:  int,
@@ -90,6 +346,7 @@ class Snipe:
             'operating_system_version',
             'mac_address'
         ]
+
         fields = self.get_custom_fields().json()
         for row in fields['rows']:
             field_name = re.search(r'_snipeit_(.*)_\d+', row['db_column_name'])
@@ -114,7 +371,78 @@ class Snipe:
             "accept": "application/json",
             "content-type": "application/json",
         }
+    
+    def api_request(
+            self,
+            method: str,
+            endpoint: str,
+            params: dict[str, str] | None = None,
+            json: dict[str,str] | None = None) -> requests.Response | None:
+        self.request_count += 1
+        if(self.request_count >= self.rate_limit):
+            logger.warning(Fore.YELLOW + "Max requests per minute reached. Sleeping for 60 seconds")
+            time.sleep(60) 
+            self.request_count = 0
+            logger.info(Fore.GREEN + "Request count has been reset", "Continuing", Style.RESET_ALL)
 
+        type = method.upper()
+        methods = ["GET", "POST", "PATCH", "DELETE"]
+        if(type not in methods):
+            logger.error(Fore.RED + "Unknown request type: %s" + Style.RESET_ALL, type)
+            return None
+        
+        return requests.get(f"{self.url}/api/v1{endpoint}", headers=self.headers, params=params, json=json, verify=self.verify_ssl)
+
+    def get_all_hardware(self, limit: int = 50, offset: int = 0) -> list[Asset] | None:
+        print('Requesting Snipe Hardware list at url ' + self.url + "/hardware")
+        response = self.api_request("GET", "/hardware", params={"limit": str(limit), "offset": str(offset)})
+        if response is None:
+            logger.error("Failed to fetch hardware: No response from server")
+            return None
+        if not response.ok:
+            logger.error("Error fetching hardware: %s", response.text)
+            return None
+        json_response = response.json()
+        if 'rows' not in json_response:
+            logger.error("Unexpected response format: %s", json_response)
+            return None
+        assets = []
+        for row in json_response['rows']:
+            asset_model = self.get_model_by_id(row['model']['id'])
+            if asset_model is None:
+                logger.error("Failed to fetch model for asset ID %s", row['id'])
+                continue
+            del row['model']
+            asset = Asset(
+                id=row['id'],
+                name=row['name'],
+                asset_tag=row['asset_tag'],
+                serial=row['serial'],
+                status_label=Status(id=row['status_label']['id'], name=row['status_label']['name'], type=row['status_label']['type']),
+                category=Category(id=row['category']['id'], name=row['category']['name']),
+                manufacturer=Manufacturer(id=row['manufacturer']['id'], name=row['manufacturer']['name']),
+                supplier=Supplier(id=row['supplier']['id'], name=row['supplier']['name']) if row.get('supplier') else None,
+                notes=row.get('notes', ''),
+                order_number=row.get('order_number'),
+                byod=row.get('byod', False),
+                eol=row.get('eol'),
+                asset_eol_date=Timestamp(date=row.get('asset_eol_date', ''), formatted=''),
+                company=row.get('company'),
+                location=Location(id=row['location']['id'], name=row['location']['name']) if row.get('location') else None,
+                rtd_location=Location(id=row['rtd_location']['id'], name=row['rtd_location']['name']) if row.get('rtd_location') else None,
+                image=row.get('image'),
+                qr=row.get('qr'),
+                alt_barcode=row.get('alt_barcode'),
+                assigned_to=None,  # This would need to be fetched separately
+                warranty_months=int(row.get('warranty_months', 0)),
+                warranty_expires=Timestamp(date='', formatted=''),  # This would need to be parsed correctly
+                created_by=None,  # This would need to be fetched separately
+                created_at=Timestamp(date='', formatted=''),  # This would need to be parsed correctly
+                updated_at=Timestamp(date='', formatted=''),  # This would need to be parsed correctly
+                last_audit_date=Timestamp(date='', formatted=''),  # This would need to be parsed correctly
+                next_audit_date= Timestamp(date='', formatted=''),  # This would need to be parsed correctly
+            
+        
     #@property
     def list_hardware(self, serial: str) -> requests.Response:
         print('Requesting Snipe Harware list at url '+ self.url + "/hardware/byserial/")
@@ -308,31 +636,6 @@ class Snipe:
             final_payload[self.custom_fields['mac_address']] = ethernet_mac
         
         return final_payload
-
-    def snipe_it_request(self, type: str, url: str, params: dict[str, str] | None = None, json: dict[str,str] | None = None) -> requests.Response | None:
-        self.request_count += 1
-        if(self.request_count >= self.rate_limit):
-            print(Fore.YELLOW + "Max requests per minute reached. Sleeping for 60 seconds")
-            time.sleep(60) 
-            self.request_count = 0
-            print(Fore.GREEN + "Request count has been reset", "Continuing", Style.RESET_ALL)
-
-
-        if(type == "GET"):
-            print('Sending GET request to snipeit', url)
-            return requests.get(self.url + url, headers = self.headers, params = params, verify=self.verify_ssl)
-        elif(type == "POST"):
-            print('Sending POST request to snipeit', url)
-            return requests.post(self.url + url, headers = self.headers, json = json, verify=self.verify_ssl)
-        elif(type == "PATCH"):
-            print('Sending PATCH request to snipeit', url)
-            return requests.patch(self.url + url, headers = self.headers, json = json, verify=self.verify_ssl)
-        elif(type == "DELETE"):
-            print('Sending DELETE request to snipeit', url)
-            return requests.delete(self.url + url, headers = self.headers, verify=self.verify_ssl)
-        else:
-            print(Fore.RED+'Unknown request type'+Style.RESET_ALL)
-            return None
 
     def get_image_for_model(self, model_number: str) -> str:
         if self.apple_image_check:
